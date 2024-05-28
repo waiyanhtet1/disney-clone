75b48153117ee43747f1e9b578b997916373b9d7
wyh <waiyanhtet1661390@gmail.com>
2024-05-29 00:04:03 +0630

    setup: api endpoint

---
+++ src/services/apiRequest.ts
@@ -0,0 +1,12 @@
+// https://api.themoviedb.org/3/trending/all/day?api_key=f5d29e675d3c8ccb9022644b6e0d3b66
+
+import axios from "axios";
+
+const baseUrl = "https://api.themoviedb.org/3/";
+const api_key = "f5d29e675d3c8ccb9022644b6e0d3b66";
+
+const getTrendingVideos = axios.get(
+  `${baseUrl}/trending/all/day?api_key=${api_key}`
+);
+
+export default { getTrendingVideos };
